8ca1964bba80ddca4fe55a98ec901efbbf72fa5c
ElizarovAlexey <55087121+ElizarovAlexey@users.noreply.github.com>
2021-02-25 20:19:21 +0200

    Add files via upload

---
+++ excercises/Объекты (1 - 10)/ex6/main.js
@@ -0,0 +1,12 @@
+const points = [
+    {x: 1, y: 2},
+     {x: 3, y: 4},
+      {x: null, y: 4},
+       {x: 3, y: undefined}
+];
+ 
+const deletePoints = (obj) => { 
+ return obj.filter((value, index) => typeof (obj[index].x) === 'number' && typeof (obj[index].y) === 'number'); 
+} 
+ 
+console.log(deletePoints(points));
