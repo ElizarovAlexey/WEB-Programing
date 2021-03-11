3ea5ba72758ee1fb913896b0f0db213e8cd0f162
ElizarovAlexey <55087121+ElizarovAlexey@users.noreply.github.com>
2021-03-11 08:58:08 +0200

    Add files via upload

--- excercises/ООП (1 - 11)/ex4/main.js
+++ excercises/ООП (1 - 11)/ex4/main.js
@@ -1,39 +1,25 @@
 class Validator {
-    
-    emailTemplate = /\S+@\S+\.\S+/;
-    domainTemplate = /\S+\.\S+/;
-    dateTemplate = /^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,4}$/;
 
-    constructor(email, domain, date) {
-        this.email = email;
-        this.domain = domain;
-        this.date = date;
+    static isEmail(email) {
+        const emailTemplate = /\S+@\S+\.\S+/;
+
+        return emailTemplate.test(email)
     }
 
-    isEmail() {
-        if (this.emailTemplate.test(this.email)) {
-            return 'Валидация почты прошла успешно';
-        }
-        return 'Ошибка валидации почты';
+    static isDomain(domain) {
+        const domainTemplate = /\S+\.\S+/;
+
+        return domainTemplate.test(domain);
     }
 
-    isDomain() {
-        if (this.domainTemplate.test(this.domain)) {
-            return 'Валидация домена прошла успешно';
-        }
-        return 'Ошибка валидации домена';
-    }
+    static isDate(date) {
+        const dateTemplate = /^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,4}$/;
 
-    isDate() {
-        if (this.dateTemplate.test(this.date)) {
-            return 'Валидация даты прошла успешно';
-        }
-        return 'Ошибка валидации даты';
+        return dateTemplate.test(date);
     }
 }
 
-let validator = new Validator('test.email@gmail.com', 'sitecom.com', '02.02.2020');
-console.log(validator.isEmail());
-console.log(validator.isDomain());
-console.log(validator.isDate());
+console.log(Validator.isEmail('test.email@gmail.com'));
+console.log(Validator.isDomain('sitecom.com'));
+console.log(Validator.isDate('02.02.2020'));
 
